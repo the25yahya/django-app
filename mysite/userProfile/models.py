a8bee7066d47a56cf2494e8c5d9d3a3ff7e2a20a
the25yahya <yahyaraja.2@gmail.com>
2024-08-09 21:25:31 +0100

    personal template

--- mysite/userProfile/models.py
+++ mysite/userProfile/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    full_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    birthday = models.DateField()
+    home_town = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'personal'
--- mysite/userProfile/views.py
+++ mysite/userProfile/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import UserProfile
 
 # Create your views here.
 def userProfile(request):
-    context = {'full-name':'yahya'}
+    email = 'the25yahya@wearehackerone.com'
+    try:
+        user = UserProfile.objects.get(email=email)
+    except UserProfile.DoesNotExist as error:
+        print(f"user not found : {error}") 
+    context = {'user':user}
     return render(request,'userProfile.html',context)
